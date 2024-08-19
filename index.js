97bcc74d24065377260662d1114fb2254054e3b4
Raul Garcia <90820418+therealgibby@users.noreply.github.com>
2024-08-18 20:11:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 window.onscroll = function () {
-	applySticky();
+	applyStickyNavbar();
 };
 
 const navbar = document.querySelector(".navbar");
+const themeMetatag = document.querySelector(`meta[name="theme-color"]`);
 
-function applySticky() {
+function applyStickyNavbar() {
 	if (window.scrollY > 0) {
 		navbar.classList.add("navbar-sticky");
+		themeMetatag.setAttribute("content", "#ffffff");
 	} else {
 		navbar.classList.remove("navbar-sticky");
+		themeMetatag.setAttribute("content", "#f54916");
 	}
 }
